056108ab6bcc6b7ee3f7cb2f94817851fc43914e
Ryan Lov <129926398+rllov@users.noreply.github.com>
2025-06-04 12:47:08 -0700

    Update target viteconfig

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,18 +3,20 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig(({ mode }) => {
-  // Load env file and merge into process.env
   const env = loadEnv(mode, process.cwd(), "");
   return {
     plugins: [react(), tailwindcss()],
-    server: {
-      proxy: {
-        "/api": {
-          target: env.VITE_API_URL || "http://localhost:8000",
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, ""),
-        },
-      },
-    },
+    server:
+      mode === "development"
+        ? {
+            proxy: {
+              "/api": {
+                target: env.VITE_API_URL,
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, ""),
+              },
+            },
+          }
+        : {},
   };
 });
